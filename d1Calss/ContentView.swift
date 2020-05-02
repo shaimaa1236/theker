09e0059d9ee2edd83f584fa67d8862513ed270fa
shaimaa <sh1611111144@outlook.com>
2020-05-02 05:47:51 +0300

    Initial Commit

---
+++ d1Calss/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  d1Calss
+//
+//  Created by Shaimaa on 5/2/20.
+//  Copyright © 2020 Shaimaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
